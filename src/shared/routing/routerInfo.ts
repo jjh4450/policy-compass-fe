b78fd472f3904c38c866bf47828ab6f18d3901dc
jjh4450 <jjh4450git@gmail.com>
2025-11-15 03:15:14 +0900

    refactor

--- src/shared/routing/routerInfo.ts
+++ src/shared/routing/routerInfo.ts
@@ -4,13 +4,13 @@ export const routerInfo: routerInfoType[] = [
   {
     path: "/",
     element: import("@/pages/Default"),
-    korean: "메인",
+    korean: "가입하기",
     expose: true,
   },
   {
     path: "/mypage",
     element: import("@/pages/MyPage"),
-    korean: "마이페이지-test",
+    korean: "회사정보",
     expose: true,
     isProtected: true,
   },
